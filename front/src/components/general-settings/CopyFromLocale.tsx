d9eaee241d29092c6de5e84302e57dae0421f4fe
Vlad Lyamzin <vlad.lyamzin@vlyamzin-mbp.local>
2021-05-13 16:35:24 +0300

    Added comments to copy logic

--- front/src/components/general-settings/CopyFromLocale.tsx
+++ front/src/components/general-settings/CopyFromLocale.tsx
@@ -22,15 +22,23 @@ function CopyFromLocale(props: CopyFromLocaleProps) {
 
   async function copy(): Promise<void> {
     const {customization: c} = config;
+    // Customization config is a set of rules
+    // Each rule has a list of locales and values pairs
+    // Here we run through the list of rules
     for (let [type, configPart] of Object.entries(c)) {
+      // Get a rule that match selected locale. Values from this config we will copy
       const configToCopy = configPart.find((item: PlatformCustomizationItem<string>) => item.locale === locale);
+      // Go to the next rule in case this one does not have required locale
       if (!configToCopy) {
         continue;
       }
+      // Check if a rule already has the locale that we are going to modify
       const configToUpdateIndex = configPart.findIndex((item: PlatformCustomizationItem<string>) => item.locale === config.selectedLocale);
+      // if there is a rule, then update it
       if (configToUpdateIndex > -1) {
         configPart[configToUpdateIndex].value = configToCopy.value;
       } else {
+        // otherwise create a new locale/value pair
         configPart.push({
           locale: config.selectedLocale,
           value: configToCopy.value
