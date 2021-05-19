59d5389234199435b791646fda6093af96b8619c
Vlad Lyamzin <vlad.lyamzin@avenga.com>
2021-05-19 11:55:52 +0300

    Added file upload hook

--- front/src/components/login-page/Logo.tsx
+++ front/src/components/login-page/Logo.tsx
@@ -1,67 +1,11 @@
-import {useConfig} from "../../services/config.provider";
 import {Button, Upload} from "antd";
 import {environment} from "../../environment";
-import {RcFile, UploadChangeParam} from "antd/es/upload";
-import {useMemo, useState} from "react";
-import fileUploadService from "../../services/file-upload.service";
-import {UploadFile} from "antd/es/upload/interface";
+import {useFileUpload} from "../../hooks/file-upload.hook";
 
-interface LogoProps {
-
-}
+interface LogoProps {}
 
 function Logo(props: LogoProps) {
-  const {config, setConfig} = useConfig();
-  const [fileList, setFile] = useState(getFileFromUrl());
-
-  useMemo(() => {
-   setFile([...getFileFromUrl()]);
-  }, [config.selectedLocale])
-
-
-  function getPathTail(path: string, delimiter: string): string {
-    const tail = path.split(delimiter).pop();
-    return tail || '';
-  }
-
-  function getFileFromUrl(): Array<any> {
-    const logoUrl = config.customization.loginPageLogoUrl.find(i => i.locale === config.selectedLocale);
-
-    if (logoUrl && logoUrl.value.length > 0) {
-      return [
-        {
-          uid: '-' + Math.floor(Math.random() * 100),
-          name: getPathTail(logoUrl.value, '/'),
-          status: 'done',
-          url: `${environment.remote}/${logoUrl.value}`,
-          thumbUrl: `${environment.remote}/${logoUrl.value}`
-        }
-      ]
-    }
-
-    return [];
-  }
-
-  function onChange(info: UploadChangeParam): void {
-    // workaround for issue https://github.com/ant-design/ant-design/issues/2423
-    setFile([...info.fileList]);
-
-    if (info?.file.status === 'done') {
-      const logo = config.customization.loginPageLogoUrl.find(i => i.locale === config.selectedLocale);
-      logo
-        ? logo.value = info.file.response.path
-        : config.customization.loginPageLogoUrl.push({locale: config.selectedLocale as string, value: info.file.response.path});
-      setConfig({...config});
-    }
-
-    if (info?.file.status === 'removed') {
-      const index = config.customization.loginPageLogoUrl.findIndex(i => i.locale === config.selectedLocale);
-      if (index > -1) {
-        config.customization.loginPageLogoUrl.splice(index, 1);
-        setConfig({...config});
-      }
-    }
-  }
+  const [fileList, uploadFile] = useFileUpload("loginPageLogoUrl");
 
   return (
     <div>
@@ -71,8 +15,7 @@ function Logo(props: LogoProps) {
               withCredentials={true}
               fileList={[...fileList]}
               maxCount={1}
-              onChange={onChange}
-      >
+              onChange={uploadFile}>
         <label className='selectLabel inline' htmlFor="logo-upload">Logo:</label>
         <Button type={'primary'} id='logo-upload'>Upload</Button>
       </Upload>
---
+++ front/src/hooks/file-upload.hook.ts
@@ -0,0 +1,63 @@
+import {useMemo, useState} from "react";
+import {UploadChangeParam} from "antd/es/upload";
+import {environment} from "../environment";
+import {useConfig} from "../services/config.provider";
+import {PlatformCustomization} from "../services/platform.service";
+import {UploadFile} from "antd/es/upload/interface";
+
+type FileUploadHook = [UploadFile<any>[], (i: UploadChangeParam) => void];
+
+export function useFileUpload(type: keyof PlatformCustomization): FileUploadHook {
+  const {config, setConfig} = useConfig();
+  const [fileList, setFile] = useState(getFileFromUrl());
+
+  useMemo(() => {
+    setFile([...getFileFromUrl()]);
+  }, [config.selectedLocale])
+
+  function getPathTail(path: string, delimiter: string): string {
+    const tail = path.split(delimiter).pop();
+    return tail || '';
+  }
+
+  function getFileFromUrl(): Array<any> {
+    const logoUrl = config.customization[type].find(i => i.locale === config.selectedLocale);
+
+    if (logoUrl && logoUrl.value.length > 0) {
+      return [
+        {
+          uid: '-' + Math.floor(Math.random() * 100),
+          name: getPathTail(logoUrl.value, '/'),
+          status: 'done',
+          url: `${environment.remote}/${logoUrl.value}`,
+          thumbUrl: `${environment.remote}/${logoUrl.value}`
+        }
+      ]
+    }
+
+    return [];
+  }
+
+  function uploadFile(info: UploadChangeParam): void {
+    // workaround for issue https://github.com/ant-design/ant-design/issues/2423
+    setFile([...info.fileList]);
+
+    if (info?.file.status === 'done') {
+      const logo = config.customization[type].find(i => i.locale === config.selectedLocale);
+      logo
+        ? logo.value = info.file.response.path
+        : config.customization[type].push({locale: config.selectedLocale as string, value: info.file.response.path});
+      setConfig({...config});
+    }
+
+    if (info?.file.status === 'removed') {
+      const index = config.customization[type].findIndex(i => i.locale === config.selectedLocale);
+      if (index > -1) {
+        config.customization[type].splice(index, 1);
+        setConfig({...config});
+      }
+    }
+  }
+
+  return [fileList, uploadFile];
+}
